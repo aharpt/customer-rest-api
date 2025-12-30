fb2e48404373e83e1112dcb7741722ba52f1ebc4
Aaron Harpt <aaron.harpt@gmail.com>
2025-12-30 14:53:57 -0600

    Ship .jar file to release branch

--- src/test/java/com/java/rest_api/RestApiApplicationTests.java
+++ src/test/java/com/java/rest_api/RestApiApplicationTests.java
@@ -1,13 +1,8 @@
 package com.java.rest_api;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class RestApiApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
